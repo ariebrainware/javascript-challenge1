8d907b48dd5d4d05e30788b61ec41d2c55369879
Arie Brainware <ariebrainware@gmail.com>
2018-07-26 09:33:01 +0700

    Web local storage function fixed, add background color to address-book

--- address-book/index.js
+++ address-book/index.js
@@ -1,63 +1,58 @@
-// // Example starter JavaScript for disabling form submissions if there are invalid fields
-// (function () {
-//     'use strict';
-//     window.addEventListener('load', function () {
-//         // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//         var forms = document.getElementsByClassName('needs-validation');
-//         // Loop over them and prevent submission
-//         var validation = Array.prototype.filter.call(forms, function (form) {
-//             form.addEventListener('submit', function (event) {
-//                 if (form.checkValidity() === false) {
-//                     event.preventDefault();
-//                     event.stopPropagation();
-//                 } else
-//                     form.classList.add('was-validated');
+const formAdd = document.getElementById('form-add')
+let dataPeoples = [] //Set empty array to handle user data input
 
-//             }, false);
-//         });
-//     }, false);
+//Form validation [Bootstrap 4]
+// (function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        var forms = document.getElementsByClassName('needs-validation');
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                event.preventDefault()
+                if (form.checkValidity() === false) {
+                    event.stopPropagation();
+                }else{
+                    addToDOM()
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
 // })();
 
-const formAdd = document.getElementById('form-add')
-const resultField = document.getElementById('result-field')
 
-const save = (firstname, lastname, address, phoneNumber, email) => {
-    window.localStorage.setItem('firstname', firstname)
-    window.localStorage.setItem('lastname', lastname)
-    window.localStorage.setItem('address', address)
-    window.localStorage.setItem('phoneNumber', phoneNumber)
-    window.localStorage.setItem('email', email)
-    console.log('Successfully save the data')
 
+//Function to call the data from local data storage
+const showPeoples = () =>{
+    const localStoragePeoples = window.localStorage.getItem('peoples')
+    dataPeoples = JSON.parse(localStoragePeoples) || []
+    dataPeoples.forEach(people => {
+        
+        let divChild = document.createElement('div')
+        let divChildInner = '';
+        for (var key in people) {
+            divChildInner += `${key}: ${people[key]}<br>`
+        }
+        divChild.innerHTML = `<b>People</b><br> ${divChildInner}`
+        let div = document.getElementById('result-field');
+        div.appendChild(divChild)  
+    })
+}
+
+//DOM Function 
+const addToDOM = () => {
+    const peoples = {
+        firstname: document.getElementById('firstname').value,
+        lastname: document.getElementById('lastname').value,
+        address: document.getElementById('address').value,
+        phoneNumber: document.getElementById('phone-number').value,
+        email: document.getElementById('email').value
+    }
+    dataPeoples.push(peoples)
+    const result= JSON.stringify(dataPeoples)
     
-
-
+    // console.log(dataPeoples)
+    window.localStorage.setItem('peoples',result)
+    showPeoples()    
 }
-
-const addToDOM = (x) => {
-    x.preventDefault()
-   
-    const firstname = document.getElementById('firstname').value
-    const lastname = document.getElementById('lastname').value
-    const address = document.getElementById('address').value
-    const phoneNumber = document.getElementById('phone-number').value
-    const email = document.getElementById('email').value
-    save(firstname, lastname, address, phoneNumber, email)
-
-    const fName = window.localStorage.getItem('firstname')
-    const lName = window.localStorage.getItem('lastname')
-    const address1 = window.localStorage.getItem('address')
-    const pNumber = window.localStorage.getItem('phoneNumber')
-    const email1 = window.localStorage.getItem('email')
-    const base = `<div>Firstname : ${fName}</div>
-<div>Lastname : ${lName}</div>
-<div>Address : ${address1}</div>
-<div>Phone Number : ${pNumber}</div>
-<div>Email : ${email1}</div>`
-
-    let div = document.createElement('div');
-    div.innerHTML = base
-    resultField.appendChild(div)
-}
-
-formAdd.addEventListener('submit', addToDOM)
+showPeoples()
